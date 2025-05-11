1f7fde55050b9d5681779087d137fc989a1897c6
Cody Moore <codymmoore97@gmail.com>
2025-05-11 18:32:39 -0500

    Make username and email unique

--- server/db/migrations/20250508233635_create_user_table.sql
+++ server/db/migrations/20250508233635_create_user_table.sql
@@ -2,8 +2,8 @@
 -- +goose StatementBegin
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(15) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    username VARCHAR(15) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
     is_active BOOLEAN DEFAULT true NOT NULL,
     is_verified BOOLEAN DEFAULT false NOT NULL,
