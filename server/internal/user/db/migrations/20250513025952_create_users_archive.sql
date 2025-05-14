38a71c692102953ab23c7555be74a5facbb3add0
Cody Moore <codymmoore97@gmail.com>
2025-05-13 20:51:08 -0500

    Remove is_active column and create users_archive table

---
+++ server/internal/user/db/migrations/20250513025952_create_users_archive.sql
@@ -0,0 +1,19 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE users_archive (
+   id SERIAL PRIMARY KEY,
+   users_id INTEGER NOT NULL,
+   username VARCHAR(15) NOT NULL,
+   email VARCHAR(255) NOT NULL,
+   password_hash TEXT NOT NULL,
+   is_verified BOOLEAN NOT NULL,
+   created_at TIMESTAMP WITH TIME ZONE,
+   updated_at TIMESTAMP WITH TIME ZONE,
+   archived_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE users_archive;
+-- +goose StatementEnd
