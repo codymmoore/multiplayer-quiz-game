1774d8ffc19b45021a4fc7ea1d0a0abd3f4629cc
Cody Moore <codymmoore97@gmail.com>
2025-06-15 20:45:51 -0500

    Validation changes

--- server/internal/user/validation.go
+++ server/internal/user/validation.go
@@ -1,21 +1,23 @@
 package user
 
 import (
-    "context"
-    "errors"
-    "fmt"
-    "reflect"
-    "regexp"
-    "strings"
-    "user/db/generated"
-    "user/dto"
+	"common"
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"reflect"
+	"regexp"
+	"strings"
+	"user/db/generated"
+	"user/dto"
 )
 
 const (
-    MinUsernameLength = 3
-    MaxUsernameLength = 15
-    MinPasswordLength = 15
-    MaxPasswordLength = 64
+	MinUsernameLength = 3
+	MaxUsernameLength = 15
+	MinPasswordLength = 15
+	MaxPasswordLength = 64
 )
 
 var usernameRegex = regexp.MustCompile(`^[a-zA-Z0-9_-]{3,15}$`)
@@ -27,143 +29,207 @@ var hasSymbol = regexp.MustCompile(`[#?!@$%^&*-]`)
 
 // ValidateCreateUserRequest Validate request for creating a new user
 func ValidateCreateUserRequest(request *dto.CreateUserRequest, service Service, context context.Context) error {
-    err := ValidateUsername(request.Username, service, context)
-    if err != nil {
-        return err
-    }
+	if err := validateUsername(request.Username, service, context); err != nil {
+		return err
+	}
 
-    err = ValidateEmail(request.Email, service, context)
-    if err != nil {
-        return err
-    }
+	if err := validateEmail(request.Email, service, context); err != nil {
+		return err
+	}
 
-    err = ValidatePassword(request.Password)
-    if err != nil {
-        return err
-    }
+	if err := validatePassword(request.Password); err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
 
 // ValidateGetUserRequest Validate request for retrieving a user
 func ValidateGetUserRequest(request *dto.GetUserRequest) error {
-    if request.UserId == nil && request.Username == nil && request.Email == nil {
-        return errors.New("ID, username, or email is required")
-    }
-    return nil
+	if request.UserId == nil && request.Username == nil && request.Email == nil {
+		return &common.HTTPError{
+			StatusCode: http.StatusBadRequest,
+			Message:    "ID, username, or email is required",
+		}
+	}
+	return nil
 }
 
 // ValidateGetUsersRequest Validate request for retrieving paginated users
 func ValidateGetUsersRequest(request *dto.GetUsersRequest) error {
-    userType := reflect.TypeOf(db.User{})
+	userType := reflect.TypeOf(db.User{})
 
-    if request.Limit != nil && *request.Limit <= 0 {
-        return errors.New("limit must be greater than zero")
-    }
+	if request.Limit != nil && *request.Limit <= 0 {
+		return &common.HTTPError{
+			StatusCode: http.StatusBadRequest,
+			Message:    "limit must be a positive number",
+		}
+	}
 
-    if request.Offset != nil && *request.Offset < 0 {
-        return errors.New("offset must be a positive number")
-    }
+	if request.Offset != nil && *request.Offset < 0 {
+		return &common.HTTPError{
+			StatusCode: http.StatusBadRequest,
+			Message:    "offset must be a positive number",
+		}
+	}
 
-    if request.SortField != nil {
-        sortFieldExists := false
-        for i := 0; i < userType.NumField(); i++ {
-            field := userType.Field(i)
-            if strings.EqualFold(field.Name, *request.SortField) {
-                sortFieldExists = true
-                break
-            }
-        }
+	if request.SortField != nil {
+		sortFieldExists := false
+		for i := 0; i < userType.NumField(); i++ {
+			field := userType.Field(i)
+			if strings.EqualFold(field.Name, *request.SortField) {
+				sortFieldExists = true
+				break
+			}
+		}
 
-        if !sortFieldExists {
-            return errors.New("invalid sort field")
-        }
-    }
+		if !sortFieldExists {
+			return &common.HTTPError{
+				StatusCode: http.StatusBadRequest,
+				Message:    "Invalid sort field",
+			}
+		}
+	}
 
-    if request.SortField != nil && (!strings.EqualFold(
-        *request.SortField,
-        "desc",
-    ) || !strings.EqualFold(*request.SortField, "asc")) {
-        return errors.New("invalid sort direction")
-    }
+	if request.SortField != nil && (!strings.EqualFold(
+		*request.SortField,
+		"desc",
+	) || !strings.EqualFold(*request.SortField, "asc")) {
+		return errors.New("invalid sort direction")
+	}
 
-    return nil
+	return nil
 }
 
 // ValidateUpdateUserRequest Validate request for updating a user
 func ValidateUpdateUserRequest(request *dto.UpdateUserRequest, service Service, context context.Context) error {
-    if request.Username != nil {
-        err := ValidateUsername(*request.Username, service, context)
-        if err != nil {
-            return err
-        }
-    }
+	if request.UserId < 0 {
+		return &common.HTTPError{
+			StatusCode: http.StatusBadRequest,
+			Message:    "Invalid user id",
+		}
+	}
 
-    if request.Email != nil {
-        err := ValidateEmail(*request.Email, service, context)
-        if err != nil {
-            return err
-        }
-    }
+	getUserRequest := dto.GetUserRequest{UserId: &request.UserId}
+	if response, _ := service.GetUser(context, &getUserRequest); response == nil {
+		return &common.HTTPError{
+			StatusCode: http.StatusNotFound,
+			Message:    "User not found",
+		}
+	}
 
-    if request.Password != nil {
-        err := ValidatePassword(*request.Password)
-        if err != nil {
-            return err
-        }
-    }
+	if request.Username != nil {
+		if err := validateUsername(*request.Username, service, context); err != nil {
+			return err
+		}
+	}
 
-    return nil
+	if request.Email != nil {
+		if err := validateEmail(*request.Email, service, context); err != nil {
+			return err
+		}
+	}
+
+	if request.Password != nil {
+		if err := validatePassword(*request.Password); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
-// ValidateUsername Validate a username
-func ValidateUsername(username string, service Service, context context.Context) error {
-    if len(username) < MinUsernameLength || len(username) > MaxUsernameLength {
-        return fmt.Errorf("username must be between %d and %d characters", MinUsernameLength, MaxUsernameLength)
-    }
+// ValidateDeleteUserRequest Validate request for deleting a user
+func ValidateDeleteUserRequest(request *dto.DeleteUserRequest, service Service, context context.Context) error {
+	if request.UserId < 0 {
+		return &common.HTTPError{
+			StatusCode: http.StatusBadRequest,
+			Message:    "Invalid user id",
+		}
+	}
 
-    if !usernameRegex.MatchString(username) {
-        return errors.New("invalid username format. username must contain only letters, numbers, underscores, and hyphens")
-    }
+	getUserRequest := dto.GetUserRequest{UserId: &request.UserId}
+	if response, _ := service.GetUser(context, &getUserRequest); response == nil {
+		return &common.HTTPError{
+			StatusCode: http.StatusNotFound,
+			Message:    "User not found",
+		}
+	}
 
-    getUserRequest := dto.GetUserRequest{Username: &username}
-    response, _ := service.GetUser(context, &getUserRequest)
-
-    if response != nil {
-        return errors.New("username already exists")
-    }
-
-    return nil
+	return nil
 }
 
-// ValidateEmail Validate an email address
-func ValidateEmail(email string, service Service, context context.Context) error {
-    if !emailRegex.MatchString(email) {
-        return errors.New("invalid email format")
-    }
+// validateUsername Validate a username
+func validateUsername(username string, service Service, context context.Context) error {
+	if len(username) < MinUsernameLength || len(username) > MaxUsernameLength {
+		return &common.HTTPError{
+			StatusCode: http.StatusBadRequest,
+			Message: fmt.Sprintf(
+				"username must be between %d and %d characters",
+				MinUsernameLength,
+				MaxUsernameLength,
+			),
+		}
+	}
 
-    getUserRequest := dto.GetUserRequest{Email: &email}
-    response, _ := service.GetUser(context, &getUserRequest)
+	if !usernameRegex.MatchString(username) {
+		return &common.HTTPError{
+			StatusCode: http.StatusBadRequest,
+			Message:    "Invalid username format. username must contain only letters, numbers, underscores, and hyphens",
+		}
+	}
 
-    if response != nil {
-        return errors.New("email already exists")
-    }
+	getUserRequest := dto.GetUserRequest{Username: &username}
+	if response, _ := service.GetUser(context, &getUserRequest); response != nil {
+		return &common.HTTPError{
+			StatusCode: http.StatusBadRequest,
+			Message:    "username already exists",
+		}
+	}
 
-    return nil
+	return nil
 }
 
-// ValidatePassword Validate a password
-func ValidatePassword(password string) error {
-    if len(password) < MinPasswordLength || len(password) > MaxPasswordLength {
-        return fmt.Errorf("password must be between %d and %d characters", MinPasswordLength, MaxPasswordLength)
-    }
+// validateEmail Validate an email address
+func validateEmail(email string, service Service, context context.Context) error {
+	if !emailRegex.MatchString(email) {
+		return &common.HTTPError{
+			StatusCode: http.StatusBadRequest,
+			Message:    "Invalid email format",
+		}
+	}
 
-    if !hasUpper.MatchString(password) || !hasLower.MatchString(password) || !hasNumber.MatchString(password) || !hasSymbol.MatchString(password) {
-        return errors.New(
-            "invalid password. Must contain at least one of the following: upper" +
-                " case English character, lower case English character, number, special character",
-        )
-    }
+	getUserRequest := dto.GetUserRequest{Email: &email}
+	if response, _ := service.GetUser(context, &getUserRequest); response != nil {
+		return &common.HTTPError{
+			StatusCode: http.StatusBadRequest,
+			Message:    "email already exists",
+		}
+	}
 
-    return nil
+	return nil
+}
+
+// validatePassword Validate a password
+func validatePassword(password string) error {
+	if len(password) < MinPasswordLength || len(password) > MaxPasswordLength {
+		return &common.HTTPError{
+			StatusCode: http.StatusBadRequest,
+			Message: fmt.Sprintf(
+				"password must be between %d and %d characters",
+				MinPasswordLength,
+				MaxPasswordLength,
+			),
+		}
+	}
+
+	if !hasUpper.MatchString(password) || !hasLower.MatchString(password) || !hasNumber.MatchString(password) || !hasSymbol.MatchString(password) {
+		return &common.HTTPError{
+			StatusCode: http.StatusBadRequest,
+			Message: "invalid password. Must contain at least one of the following: upper" +
+				" case English character, lower case English character, number, special character",
+		}
+	}
+
+	return nil
 }
