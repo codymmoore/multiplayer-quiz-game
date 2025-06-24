81d62d1316374b558ba6f36dcd1f5ebb06abdeca
Cody Moore <codymmoore97@gmail.com>
2025-06-23 21:30:42 -0500

    Add common User struct for response structs

--- server/internal/user/dto/response.go
+++ server/internal/user/dto/response.go
@@ -8,7 +8,7 @@ type CreateUserResponse struct {
 	UserId int `json:"userId"`
 }
 
-type GetUserResponse struct {
+type User struct {
 	UserId       int       `json:"userId"`
 	Username     string    `json:"username"`
 	Email        string    `json:"email"`
@@ -17,22 +17,16 @@ type GetUserResponse struct {
 	CreatedAt    time.Time `json:"createdAt"`
 	UpdatedAt    time.Time `json:"updatedAt"`
 }
+
+type GetUserResponse = User
 
 type GetUsersResponse struct {
-	Users    []GetUserResponse `json:"users"`
-	PrevLink *string           `json:"prevLink,omitempty"`
-	NextLink *string           `json:"nextLink,omitempty"`
+	Users    []User  `json:"users"`
+	PrevLink *string `json:"prevLink,omitempty"`
+	NextLink *string `json:"nextLink,omitempty"`
 }
 
-type UpdateUserResponse struct {
-	UserId       int       `json:"userId"`
-	Username     string    `json:"username"`
-	Email        string    `json:"email"`
-	PasswordHash string    `json:"passwordHash"`
-	IsVerified   bool      `json:"isVerified"`
-	CreatedAt    time.Time `json:"createdAt"`
-	UpdatedAt    time.Time `json:"updatedAt"`
-}
+type UpdateUserResponse = User
 
 type DeleteUserResponse struct {
 }
