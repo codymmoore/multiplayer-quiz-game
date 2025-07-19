ae3ccaddb0236c97a0ce78fe9095d3bd75ce58d6
Cody Moore <codymmoore97@gmail.com>
2025-07-19 18:49:45 -0500

    Add test.sh

---
+++ test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -euo pipefail
+
+PROJECT_ROOT="$(pwd)"
+SERVICES=("user")
+
+echo "Running unit tests..."
+for SERVICE in "${SERVICES[@]}"; do
+    SERVICE_PATH="./server/internal/$SERVICE"
+
+    cd $SERVICE_PATH
+
+    echo "---------- ${SERVICES} ----------"
+    go test -v
+
+    cd $PROJECT_ROOT
+done
+
+echo -e "\nBuilding and starting docker containers..."
+cd server
+docker compose up --build -d
+cd $PROJECT_ROOT
+
+echo -e "\nRunning Postman tests..."
+for SERVICE in "${SERVICES[@]}"; do
+    echo "---------- ${SERVICES} ----------"
+    newman run "postman/collection/Quizchief - ${SERVICE^} Service.postman_collection.json" \
+        --environment "postman/environment/Quizchief - Local.postman_environment.json" \
+        --reporters cli,json
+done
