00082fdfdd09a101efbd87a52385f8c0343008da
Cody Moore <codymmoore97@gmail.com>
2025-07-21 00:50:07 -0500

    Make created_at and updated_at in users table required

--- server/internal/user/db/migrations/00005_create_users_archive.sql
+++ server/internal/user/db/migrations/00005_create_users_archive.sql
@@ -7,8 +7,8 @@ CREATE TABLE users_archive (
    email VARCHAR(255) NOT NULL,
    password_hash TEXT NOT NULL,
    is_verified BOOLEAN NOT NULL,
-   created_at TIMESTAMP WITH TIME ZONE,
-   updated_at TIMESTAMP WITH TIME ZONE,
+   created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+   updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
    archived_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 -- +goose StatementEnd
