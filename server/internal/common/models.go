19223a800fb1bd263e6996fd7a1e963d808eb738
Cody Moore <codymmoore97@gmail.com>
2025-07-21 00:48:13 -0500

    Move AuthMiddleware to common package

--- server/internal/common/models.go
+++ server/internal/common/models.go
@@ -1,8 +1,22 @@
 package common
 
+import (
+	"time"
+)
+
 // UserClaims Stores JWT information for User
 type UserClaims struct {
 	ID       int    `json:"user_id"`
 	Username string `json:"username"`
 	Email    string `json:"email"`
+}
+
+type getUserResponse struct {
+	UserId       int       `json:"userId"`
+	Username     string    `json:"username"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"passwordHash"`
+	IsVerified   bool      `json:"isVerified"`
+	CreatedAt    time.Time `json:"createdAt"`
+	UpdatedAt    time.Time `json:"updatedAt"`
 }
